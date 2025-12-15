09d0638bf8e18c3778a14da9b51d0b94b09bd190
ryaabe <ryaabe2002@gmail.com>
2025-12-15 02:47:46 -0800

    fix app settings.py

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "apps.tracker",
+    "apps.tracker.apps.TrackerConfig",
 ]
 
 MIDDLEWARE = [
